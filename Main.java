3f4392e08de56d40f8114d1bb13aa7e1f5a7dc20
ayushig2004 <154684947+ayushig2004@users.noreply.github.com>
2025-04-23 10:24:37 +0530

    Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); // Corrected: System.in
+        Scanner sc = new Scanner(System.in); 
         int chances = 6;
         int finals = 0;
         boolean playAgain = true;
@@ -29,8 +29,8 @@ public class Main {
                 System.out.println("Sorry Player You lost the chance.The number is " + ran);
             }
             System.out.println("Do you want to play Again (y/n)");
-            String PA = sc.next(); // Corrected: sc.next() to get String
-            playAgain = PA.equalsIgnoreCase("y"); // Corrected: equalsIgnoreCase
+            String PA = sc.next(); 
+            playAgain = PA.equalsIgnoreCase("y");
         }
         System.out.println("That's it Player, Hope you enjoyed it");
         System.out.println("Here is your Score: " + finals);
